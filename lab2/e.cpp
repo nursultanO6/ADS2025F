27889be395e37a09126c4c971d146bdd29420fbb
nursultanO6 <kenzhe12nur@gmail.com>
2025-09-13 13:29:26 +0500

    some changes

--- lab2/e.cpp
+++ lab2/e.cpp
@@ -1,14 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
+struct ListNode{
+    string val;
+    ListNode* next;
+
+    ListNode(string x){
+        val = x;
+        next = nullptr;
+    }
+};
+
+ListNode* reverse(ListNode* head){
+    ListNode* prev = nullptr;
+    while(head){
+        ListNode* temp = head->next;
+        head->next = prev;
+        prev = head;
+        head = temp;
+    }
+
+    return prev;
+}
 int main(){
-    int n; cin >> n;
-    set<string> std;
-    for(int i = 0; i < n;i++){
+    int n, sum = 0; cin >> n;
+    ListNode* head = new ListNode("");
+    ListNode* dum = head;
+    for(int i = 0;i<n;i++){
         string s; cin >> s;
-        std.insert(s);
+        if(dum ->val == s){
+            continue;
+        }
+        sum++;
+        dum->next = new ListNode(s);
+        dum = dum->next;
     }
-    cout << "All in all: " << std.size() << endl << "Students:" << endl;
-    for (string i : std) {
-        cout << i << endl;
+
+    head = reverse(head);
+
+    cout << "All in all: " << sum << endl;
+    cout << "Students: " << endl;
+
+    dum = head;
+
+    while(dum){
+        cout << dum->val << endl;
+        dum = dum->next;
     }
+
+
+    
 }
